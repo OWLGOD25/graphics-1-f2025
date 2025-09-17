6677a8dc8e84b839e171cf8797fff5b415744c70
Tyron Fajardo <tyronjf44@gmail.com>
2025-09-17 14:56:37 -0400

    adding in lab code

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,19 @@
 #include "Window.h"
 #include <glad/glad.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <array>
+
+constexpr int ScreenWidth = 1280;
+constexpr int ScreenHeight = 720;
+
+void APIENTRY GLDebugMessageCallback(GLenum source, GLenum type, unsigned int id, GLenum severity, GLsizei length, const char* message, const void* userParam);
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+
 
 int main()
 {
-    CreateWindow(800, 800, "Graphics 1");
 
-    /* Loop until the user closes the window */
-    while (!WindowShouldClose())
-    {
-        // Colors are represented as fractions between 0.0 and 1.0, so convert using a colour-picker tool accordingly!
-        float r = 239.0f / 255.0f;
-        float g = 136.0f / 255.0f;
-        float b = 190.0f / 255.0f;
-        float a = 1.0f;
-
-        /* Render here */
-        glClearColor(r, g, b, a);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        Loop();
-    }
-
-    DestroyWindow();
-    return 0;
 }
